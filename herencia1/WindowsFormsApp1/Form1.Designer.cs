df0492b35d02d05f740eeb7985901c91cbfc2b49
Isaac1614 <163430258+Isaac1614@users.noreply.github.com>
2024-04-25 09:37:45 -0600

    Add files via upload

---
+++ herencia1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Sumar = new System.Windows.Forms.Button();
+            this.btn_restar = new System.Windows.Forms.Button();
+            this.btn_multiplicar = new System.Windows.Forms.Button();
+            this.btn_dividir = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_valor1 = new System.Windows.Forms.TextBox();
+            this.textBox_valor2 = new System.Windows.Forms.TextBox();
+            this.textBox_resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Sumar
+            // 
+            this.btn_Sumar.BackColor = System.Drawing.Color.Coral;
+            this.btn_Sumar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Sumar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_Sumar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Sumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sumar.Location = new System.Drawing.Point(310, 36);
+            this.btn_Sumar.Name = "btn_Sumar";
+            this.btn_Sumar.Size = new System.Drawing.Size(97, 47);
+            this.btn_Sumar.TabIndex = 0;
+            this.btn_Sumar.Text = "Sumar";
+            this.btn_Sumar.UseVisualStyleBackColor = false;
+            // 
+            // btn_restar
+            // 
+            this.btn_restar.BackColor = System.Drawing.Color.Coral;
+            this.btn_restar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_restar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_restar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_restar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_restar.Location = new System.Drawing.Point(310, 89);
+            this.btn_restar.Name = "btn_restar";
+            this.btn_restar.Size = new System.Drawing.Size(97, 47);
+            this.btn_restar.TabIndex = 1;
+            this.btn_restar.Text = "Restar";
+            this.btn_restar.UseVisualStyleBackColor = false;
+            // 
+            // btn_multiplicar
+            // 
+            this.btn_multiplicar.BackColor = System.Drawing.Color.Coral;
+            this.btn_multiplicar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_multiplicar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_multiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_multiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_multiplicar.Location = new System.Drawing.Point(310, 142);
+            this.btn_multiplicar.Name = "btn_multiplicar";
+            this.btn_multiplicar.Size = new System.Drawing.Size(97, 47);
+            this.btn_multiplicar.TabIndex = 2;
+            this.btn_multiplicar.Text = "Multiplicar";
+            this.btn_multiplicar.UseVisualStyleBackColor = false;
+            // 
+            // btn_dividir
+            // 
+            this.btn_dividir.BackColor = System.Drawing.Color.Coral;
+            this.btn_dividir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_dividir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_dividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_dividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dividir.Location = new System.Drawing.Point(310, 195);
+            this.btn_dividir.Name = "btn_dividir";
+            this.btn_dividir.Size = new System.Drawing.Size(97, 47);
+            this.btn_dividir.TabIndex = 3;
+            this.btn_dividir.Text = "Dividir";
+            this.btn_dividir.UseVisualStyleBackColor = false;
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.BackColor = System.Drawing.Color.Coral;
+            this.btn_limpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_limpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(310, 248);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(97, 47);
+            this.btn_limpiar.TabIndex = 4;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Valor 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valor 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resultado";
+            // 
+            // textBox_valor1
+            // 
+            this.textBox_valor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_valor1.Location = new System.Drawing.Point(137, 76);
+            this.textBox_valor1.Name = "textBox_valor1";
+            this.textBox_valor1.Size = new System.Drawing.Size(100, 24);
+            this.textBox_valor1.TabIndex = 8;
+            // 
+            // textBox_valor2
+            // 
+            this.textBox_valor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_valor2.Location = new System.Drawing.Point(137, 129);
+            this.textBox_valor2.Name = "textBox_valor2";
+            this.textBox_valor2.Size = new System.Drawing.Size(100, 24);
+            this.textBox_valor2.TabIndex = 9;
+            // 
+            // textBox_resultado
+            // 
+            this.textBox_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_resultado.Location = new System.Drawing.Point(137, 182);
+            this.textBox_resultado.Name = "textBox_resultado";
+            this.textBox_resultado.Size = new System.Drawing.Size(100, 24);
+            this.textBox_resultado.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(419, 314);
+            this.Controls.Add(this.textBox_resultado);
+            this.Controls.Add(this.textBox_valor2);
+            this.Controls.Add(this.textBox_valor1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_dividir);
+            this.Controls.Add(this.btn_multiplicar);
+            this.Controls.Add(this.btn_restar);
+            this.Controls.Add(this.btn_Sumar);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Sumar;
+        private System.Windows.Forms.Button btn_restar;
+        private System.Windows.Forms.Button btn_multiplicar;
+        private System.Windows.Forms.Button btn_dividir;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_valor1;
+        private System.Windows.Forms.TextBox textBox_valor2;
+        private System.Windows.Forms.TextBox textBox_resultado;
+    }
+}
+
---
+++ herencia1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        //clase principal o clase papa que guarda los metodos de lectura
+        /*de valores almacenados de valores en variables*/
+        class operacion
+        {
+            //declaracion de variables
+            //tipo de acceso //tipo de dato // nombre de dato
+            protected int valor1;
+            protected int valor2;
+            protected int resultado;
+        }
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
